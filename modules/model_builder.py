5d0b34746221a345b78c739bfff9b5b1a375f4e9
Stefan Kolling <stefankolling@gmail.com>
2025-09-29 12:13:16 +0200

    Replace the model's final layer with a lazy linear layer which infers the input shape

--- modules/model_builder.py
+++ modules/model_builder.py
@@ -2,14 +2,14 @@
 Contains PyTorch model code to instantiate a TinyVGG model.
 """
 import torch
-from torch import nn 
+from torch import nn
 
 class TinyVGG(nn.Module):
   """Creates the TinyVGG architecture.
 
   Replicates the TinyVGG architecture from the CNN explainer website in PyTorch.
   See the original architecture here: https://poloclub.github.io/cnn-explainer/
-  
+
   Args:
     input_shape: An integer indicating number of input channels.
     hidden_units: An integer indicating number of hidden units between layers.
@@ -18,13 +18,13 @@ class TinyVGG(nn.Module):
   def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
       super().__init__() # immediately call __init__() of super class
       self.conv_block_1 = nn.Sequential(
-          nn.Conv2d(in_channels=input_shape, 
-                    out_channels=hidden_units, 
-                    kernel_size=3, 
-                    stride=1, 
-                    padding=0),  
+          nn.Conv2d(in_channels=input_shape,
+                    out_channels=hidden_units,
+                    kernel_size=3,
+                    stride=1,
+                    padding=0),
           nn.ReLU(),
-          nn.Conv2d(in_channels=hidden_units, 
+          nn.Conv2d(in_channels=hidden_units,
                     out_channels=hidden_units,
                     kernel_size=3,
                     stride=1,
@@ -42,11 +42,10 @@ class TinyVGG(nn.Module):
       )
       self.classifier = nn.Sequential(
           nn.Flatten(),
-          # Where did this in_features shape come from? 
+          # Where did this in_features shape come from?
           # It's because each layer of our network compresses and changes the shape of our inputs data.
-          nn.Linear(in_features=hidden_units*13*13,
-                    out_features=output_shape)
+          nn.LazyLinear(out_features=output_shape) # nn.LazyLinear() automatically infers in_features from input shape of the tensor coming from the previous layer; other than that identical to nn.Linear()
       )
-    
+
   def forward(self, x: torch.Tensor):
       return self.classifier(self.conv_block_2(self.conv_block_1(x))) # <- leverage the benefits of operator fusion
